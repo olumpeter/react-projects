9c57a3019b24046223e92594085e87bb02e0a1e7
Peter Olum <44267527+olumpeter@users.noreply.github.com>
2024-05-16 21:28:13 +0300

    Add files via upload

--- 001-creating-and-nesting-components/scripts/App.js
+++ 001-creating-and-nesting-components/scripts/App.js
@@ -8,17 +8,17 @@ root.render(
 
 function MyApp() {
     return (
-        <div>
+        <>
             <h1>Welcome to my app</h1>
             <MyButton />
-        </div>
+        </>
     )
 }
 
 function MyButton() {
     return (
-        <div>
+        <>
             <button>I'm a button</button>
-        </div>
+        </>
     )
 }
