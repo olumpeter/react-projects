a19090e1f62a392b215156a32416cb7db98fbb90
Peter Olum <44267527+olumpeter@users.noreply.github.com>
2024-05-18 10:54:07 +0300

    Add files via upload

---
+++ 006-rendering-lists/README.md
@@ -0,0 +1,19 @@
+
+# Rendering lists
+
+You will rely on JavaScript features like `for loop` and the array `map()` function to render lists of components.
+
+
+## How you can access this live website
+
+<dl>
+  Use the following URL:
+  <dd>
+    https://olumpeter.github.io/react-projects/005-conditional-rendering/
+  </dd>
+  or click the following link:
+  <dd>
+    <a href="https://olumpeter.github.io/react-projects/006-rendering-lists/">Visit website</a>
+  </dd>
+</dl>
+
---
+++ 006-rendering-lists/script/App.jsx
@@ -0,0 +1,35 @@
+
+const app = document.getElementById('app');
+const root = ReactDOM.createRoot(app);
+
+root.render(
+    <>
+        <h1>Rendering lists</h1>
+        <div>
+            <h2>My favorite fruits:</h2>
+            <DisplayMyFavoriteFruits />
+        </div>
+    </>
+);
+
+function DisplayMyFavoriteFruits() {
+    const myFavoriteFruits = [
+        {key: 1, name: 'Apple'},
+        {key: 2, name: 'Banana'},
+        {key: 3, name: 'Pineaple'},
+        {key: 4, name: 'Kiwi'},
+        {key: 5, name: 'Watermelon'},
+    ]
+
+    const listMyFavoriteFruits = []
+    for (const fruit of myFavoriteFruits) {
+        listMyFavoriteFruits.push(
+            <li key={fruit.key}>{fruit.name}</li>
+        )
+    }    
+    return (
+        <ul>
+            {listMyFavoriteFruits}
+        </ul>
+    )
+}
